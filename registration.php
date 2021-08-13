5c26df7b5182246bb738b4b52318bb2df29e2a79
BryceVE <0362729@schoolsnet.act.edu.au>
2021-08-13 13:09:36 +1000

    Fixed comments to registration.php file to explain what the code does.

--- registration.php
+++ registration.php
@@ -33,6 +33,7 @@
                 <p>Please enter More Personal Details:</p>
                 <p>Name<input type="text" name="name" class="form-control" required="required"></p>
                 <p>Profile Picture <input type="file" name="file" class="form-control" required="required" onchange="preview_image(event)" accept="image/*"></p>
+                <!--display profile picture image preview-->
                 <p>Image preview: <img id="output_image" width="100"></p>
             </div>
         </div>
@@ -42,16 +43,17 @@
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//  Customer Details
+//Customer Details
     $username = sanitise_data($_POST['username']);
     $password = sanitise_data($_POST['password']);
     $name = sanitise_data($_POST['name']);
 
-    //check if user exists.
+//check if user exists.
     $query = $conn->query("SELECT COUNT(*) FROM user WHERE username='$username'");
     $data = $query->fetchArray();
     $numberOfUsers = (int)$data[0];
 
+//if there a user already exists with same username display error message
     if ($numberOfUsers > 0) {
         echo "Sorry, username already taken";
     } else {
@@ -71,21 +73,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $fileType = $_FILES['file']['type'];
 
 //defining what type of file is allowed
-// We seperate the file, and obtain the end.
+//separate the file to obtain the file type.
         $fileExt = explode('.', $fileName);
         $fileActualExt = strtolower(end($fileExt));
-//We ensure the end is allowable in our thing.
+//allowed file types.
         $allowed = array('jpg', 'jpeg', 'png', 'pdf');
 
+        //if the file type is allowed
         if (in_array($fileActualExt, $allowed)) {
             if ($fileError === 0) {
-                //File is smaller than yadda.
+                //file is smaller than allowed size.
                 if ($fileSize < 10000000000) {
-                    //file name is now a unique ID based on time with IMG- precedding it, followed by the file type.
+                    //file name is now a unique ID (name) based on time with IMG- precedding it, followed by the file type.
                     $fileNameNew = uniqid('IMG-', True) . "." . $fileActualExt;
                     //upload location
                     $fileDestination = 'images/profile_pictures/' . $fileNameNew;
-                    //command to upload.
+                    //command to upload data to database.
                     move_uploaded_file($fileTmpName, $fileDestination);
                     $sql = "INSERT INTO user (username, password, name, profilePic, accessLevel) VALUES (:newUsername, :newPassword, :newName, :newImage, 'User')";
                     $stmt = $conn->prepare($sql);
@@ -94,6 +97,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $stmt->bindValue(':newName', $name);
                     $stmt->bindValue(':newImage', $fileNameNew);
                     $stmt->execute();
+                    //redirects to home page
                     header("location:index.php");
                 } else {
                     echo "Your image is too big!";
@@ -106,7 +110,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 }
-
 ?>
 
 </body>
