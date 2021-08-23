db9084e4f0b2cfa1db6aa46c321f2bf2de4f86ad
BryceVE <0362729@schoolsnet.act.edu.au>
2021-08-24 09:24:30 +1000

    Profile page displays user information if logged in.

--- profile.php
+++ profile.php
@@ -1,4 +1,43 @@
-<?php include "template.php"; ?>
+<?php include "template.php";
+/**
+ *  This is the user's profile page.
+ * It shows the Users details including picture, and a link to edit the details.
+ *
+ * @var SQLite3 $conn
+ */
+?>
 <title>User Profile</title>
 
-<h1 class='text-primary'>Profile Page</h1>
+<h1 class='text-primary'>Your Profile</h1>
+
+<?php
+//session variables into php variables
+$userName = $_SESSION["username"];
+$userId = $_SESSION["user_id"];
+
+//selects all data for user logged in
+$query = $conn->query("SELECT * FROM user WHERE username='$userName'");
+$userData = $query->fetchArray();
+//stores data into variables
+$userName = $userData[1];
+$password = $userData[2];
+$name = $userData[3];
+$profilePic = $userData[4];
+$accessLevel = $userData[5];
+?>
+
+<!--Displays the user information-->
+<div class ="container-fluid">
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Username :  <?php echo $userName; ?></h3>
+            <p>Profile Picture:</p>
+            <?php echo "<img src='images/profile_pictures/".$profilePic."' width='100' height='100'>"   ?>
+        </div>
+        <div class="col-md-6">
+            <p> Name : <?php echo $name ?> </p>
+            <p> Access Level : <?php echo $accessLevel ?> </p>
+            <p><a href="edit.php" title="Edit">Edit Profile</a></p>
+        </div>
+    </div>
+</div>
