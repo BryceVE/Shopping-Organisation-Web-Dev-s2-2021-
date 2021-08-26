6148efe8f59015a783fe90f3c82860e23c70bcd4
BryceVE <0362729@schoolsnet.act.edu.au>
2021-08-26 12:16:55 +1000

    Improved commenting on invoice.php

--- invoice.php
+++ invoice.php
@@ -1,4 +1,7 @@
-<?php include "template.php"; ?>
+<?php
+//includes the Navbar
+include "template.php"; ?>
+<!--title-->
 <title>Invoicing</title>
 <?php
 /**
@@ -13,17 +16,16 @@
  * "Defines" the conn variable, removing the undefined variable errors.
  * @var SQLite3 $conn
  */
-?>
 
-<?php
-//is there nothing in the url that says "order"
+
+//if there is nothing in the url that says "order"
 if (empty($_GET["order"])) { // Showing the list of open order (case 1)
     //headings
     echo "<h1 class='text-primary'>Invoices</h1>";
     echo "<h2 class='text-primary'>Choose your invoice number below.</h2>";
 
     //if user is logged in
-    if (isset($_SESSION["user_id"])) { // Case 1 or 3?
+    if (isset($_SESSION["user_id"])) { // Case 1
 
         //if user is an admin
         if($_SESSION["level"] =="Administrator") {
@@ -39,6 +41,7 @@ if (empty($_GET["order"])) { // Showing the list of open order (case 1)
             $count = $conn->querySingle("SELECT orderCode FROM `orderDetails` WHERE userID='$userID' AND status='OPEN'");
         }
 
+        //empty variable for later
         $orderCodesForUser = [];
 
         //if user has any open orders
@@ -48,18 +51,22 @@ if (empty($_GET["order"])) { // Showing the list of open order (case 1)
                 $orderCode = $data[0];
                 array_push($orderCodesForUser, $orderCode);
             }
+            //line break
             echo "<p>";
 
             //Gets the unique order numbers from the extracted table above.
             $unique_orders = array_unique($orderCodesForUser);
             // Produce a list of links of the Orders for the user.
+            //for each order the user has made repeat the following
             foreach ($unique_orders as $order_ID) {
+                //display the order id and make it a link to the order id
                 echo "<p><a href='invoice.php?order=" . $order_ID . "'>Order : " . $order_ID . "</a></p>";
             }
-        } else{ // Case 3
+        } else { // Case 3
             echo "You don't have any open orders. Please make an order to view them<br><br>";
             //displays button that takes user to order page
             echo '<button id="Order_page" class="btn btn-primary">Order Here</button>';?>
+<!--when the button is pressed takes the user to order page-->
 <script type="text/javascript">
     document.getElementById("Order_page").onclick = function () {
         location.href = "orderForm.php";
@@ -67,42 +74,48 @@ if (empty($_GET["order"])) { // Showing the list of open order (case 1)
 </script>
 <?php
 
-        }
-    }else {// Case 5
+        } //ends else if
+    }else { // Case 5
+        //redirects the user to the home page
         header("Location:index.php");
     }
 } else { // Case 2 - There is an order code in the URL
     $order_id = $_GET["order"];
-    //heading
+    //heading with order id in it
     echo"<h1 class='text-primary'>Invoice -". $order_id ."</h1>";
     //p = products table; o = order table
     $query = $conn->query("SELECT p.productName, p.price, o.quantity, p.price*o.quantity as SubTotal, o.orderDate, o.status FROM orderDetails o INNER JOIN products p on o.productCode = p.code WHERE orderCode='$order_id'");
 
-    //total cost
+    //stores total cost
     $total = 0;
 
     //display data
     echo"<div class='container-fluid'><div class='row'><div class='col text-success'>Product Name</div><div class='col text-success'>Price</div><div class='col text-success'>Quantity</div><div class='col text-success'>Subtotal</div></div>";
+    //while getting the data from SQL query above
     while($data = $query->fetchArray()) {
+        //create a row for each product in order
         echo"<div class='row'>";
+        //separates the data from SQL for use later
         $productName = $data[0];
         $price = $data[1];
         $quantity = $data[2];
         $subtotal = $data[3];
         $orderDate = $data[4];
         $status = $data[5];
-        //calculating total cost
+        //calculates total cost
         $total = $total + $subtotal;
+        //displays the product information from order
         echo"<div class='col'>". $productName ."</div>";
         echo"<div class='col'>$". $price ."</div>";
         echo"<div class='col'>". $quantity ."</div>";
         echo"<div class='col'>$". $subtotal ."</div>";
-
+        //finished row
         echo"</div>";
     }
 
+    //displays total cost
     echo"<div class='row'><div class='col'></div><div class='col'></div><div class='col display-4'>Total : $". $total ."</div></div>";
+    //displays when the order was submitted
     echo"<div class='row'><div class='col'></div><div class='col'></div><div class='col'>". $orderDate ."</div></div>";
-
 }
 ?>
