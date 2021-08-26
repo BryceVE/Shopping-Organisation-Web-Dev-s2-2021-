283fdf8cb9d533943ed149a8f1cf226aae8cc8e0
BryceVE <0362729@schoolsnet.act.edu.au>
2021-08-26 12:27:44 +1000

    Improved commenting on logout.php

--- logout.php
+++ logout.php
@@ -4,6 +4,9 @@ session_start();
 //unset($_SESSION["name"]);
 //unset($_SESSION["username"]);
 //unset($_SESSION["level"]);
+
+//unsets all the session data (user info)
 session_unset();
+//redirects user to home page
 header("Location:index.php");
 ?>
