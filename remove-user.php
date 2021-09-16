e244be8596a0f2e6e01e1f6e64b0b23aee667620
BryceVE <0362729@schoolsnet.act.edu.au>
2021-09-16 13:06:22 +1000

    added remove user page, not functioning yet

---
+++ remove-user.php
@@ -0,0 +1,30 @@
+<?php
+//includes the nav bar
+include "template.php";
+/**
+ * This is the remove user page.
+ * It shows the Users details including picture, and a link to edit the details.
+ *
+ * @var SQLite3 $conn
+ */
+?>
+<!--title-->
+    <title>Remove User from Database</title>
+<!--heading-->
+    <h1 class='text-primary'>Remove User from Database</h1>
+
+<?php
+// if a user is logged in
+if (isset($_GET["user_id"])) {
+    //delete user from database
+    $userToDelete = $_GET["user_id"];
+    //echo "<p>".$userToDelete."</p>";
+    $query = "DELETE FROM user WHERE user_id='$userToDelete'";
+    $sqlstmt = $conn->prepare($query);
+    $sqlstmt->execute();
+    //success message
+    echo "<p>User ".$userToDelete." has been deleted from the database";
+} else {
+    echo "No User to Delete";
+}
+?>
