9b7c7ccc4329a9e14ec874b31ce0a70cc62d06bc
BryceVE <0362729@schoolsnet.act.edu.au>
2021-10-15 10:23:56 +1100

    Created product-list.php page for admins to see every product in the database. Edit and Delete functions not working yet.

---
+++ product-list.php
@@ -0,0 +1,46 @@
+<?php
+//includes the Navbar
+include "template.php"; ?>
+<!--title-->
+<title>Products List</title>
+
+<!--heading-->
+<h1 style='margin-left: 10px' class='text-primary'>Products List</h1> <!--bootstrap colours-->
+<br>
+
+
+<?php
+//grabs all the product names and their images from the products table
+$productList = $conn->query("SELECT productName, image FROM products");
+?>
+
+<!--Display the products and their images nicely-->
+<div class="container-fluid">
+    <?php
+    //repeats the following code for each product in the database
+    while ($productData = $productList->fetchArray()) {
+        ?>
+        <div class="row">
+            <div class="col-md-2">
+                <?php
+                echo '<img src="images/product_pictures/'.$productData[1].'" height="50">';
+                ?>
+            </div>
+            <div class="col-md-4">
+                <?php echo $productData[0]; ?>
+            </div>
+            <div class="col-md-2">
+                <!--            edit button-->
+                Edit
+            </div>
+            <div class="col-md-2">
+                <!--            delete button-->
+                Delete
+            </div>
+        </div>
+        <?php
+    }
+    ?>
+
+
+</div>
